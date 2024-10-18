e477d49f50277865259f1693a523c791f6d03562
ForbiddenR <hostyouposition@outlook.com>
2024-10-18 16:14:00 +0800

    implement a simple rate limiter

---
+++ rate/rate.go
@@ -0,0 +1,32 @@
+package rate
+
+import (
+	"sync"
+	"time"
+)
+
+type Limiter struct {
+	duration time.Duration
+	actTime  time.Time
+	lock     sync.Locker
+}
+
+func NewLimiter(duration time.Duration) *Limiter {
+	limiter := &Limiter{
+		duration: duration,
+		lock:     &sync.Mutex{},
+	}
+	limiter.actTime = time.Now()
+	return limiter
+}
+
+func (l *Limiter) Allow() (time.Duration, bool) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+	now := time.Now()
+	if diff := l.actTime.Sub(now); diff > 0 {
+		return diff, false
+	}
+	l.actTime = now.Add(l.duration)
+	return time.Duration(0), true
+}
